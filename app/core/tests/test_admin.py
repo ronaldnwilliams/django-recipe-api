f7fa14d1ce106b06c1e3e5ee5ead005d15f0a3b4
Ronald Williams <ronald.williams@peerfit.com>
2019-08-01 10:57:42 -0500

    Add user to admin.

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,43 @@
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_super_user(
+            'admin@rrecipes.com',
+            'testpass123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            'testuser@testemail.com',
+            'testpass123',
+            name='Test User'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
